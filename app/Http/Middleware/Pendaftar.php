f92722fe19f1c5de684c806c7fd771d4ffe9ecdc
syifshab73@gmail.com <syifshab73@gmail.com>
2025-01-17 08:56:25 +0700

    middleware

---
+++ app/Http/Middleware/Pendaftar.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
+
+class Pembina
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (Auth::user()->peran != 'Pendaftar')
+        {
+            return back();
+        }
+        return $next($request);
+    }
+}
