e0fc019ec21276e7d361c43eac58140ead99c7c5
syifshab73@gmail.com <syifshab73@gmail.com>
2025-01-17 09:00:00 +0700

    aliasnya

--- app/Http/Middleware/Pendaftar.php
+++ app/Http/Middleware/Pendaftar.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Support\Facades\Auth;
 
-class Pembina
+class Pendaftar
 {
     /**
      * Handle an incoming request.
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'Pembina' => \App\Http\Middleware\Pembina::class,
+            'Pembimbing' => \App\Http\Middleware\Pembimbing::class,
+            'Pendaftar' => \App\Http\Middleware\Pendaftar::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
