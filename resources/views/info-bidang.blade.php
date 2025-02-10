c91f331f2b360b886c5eadbb64258800217930c7
syifshab73@gmail.com <syifshab73@gmail.com>
2025-02-10 12:15:45 +0700

    info

--- resources/views/info-bidang.blade.php
+++ resources/views/info-bidang.blade.php
@@ -3,8 +3,16 @@
 @section('title', 'Info Bidang')
 
 @section('content')
-<div class="container mx-auto py-10">
-    <h1 class="text-3xl font-bold text-center mb-6">Info Bidang</h1>
+{{-- <div class="container mx-auto py-10"> --}}
+    <div class="container mx-auto flex items-center justify-between py-30">
+        <div class="banner relative w-full h-72 bg-cover bg-center flex items-center justify-center text-white text-center mb-10" style="background-image: url('/img/buku.png');">
+            <div class="overlay absolute inset-0 bg-black bg-opacity-50"></div>
+            <div class="banner-text relative z-10">
+                <h1 class="text-4xl font-bold uppercase">Info Bidang</h1>
+                <p class="text-lg">Magang Diskominfo Kota Semarang</p>
+            </div>
+        </div>
+    </div>
     <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
         @foreach($bidang as $b)
         <div class="bg-white shadow-lg rounded-lg p-8 cursor-pointer hover:bg-gray-100 transition mx-auto w-full max-w-md"
@@ -36,7 +44,7 @@
         </div>
         @endforeach
     </div>
-</div>
+{{-- </div> --}}
 
 <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 @endsection
